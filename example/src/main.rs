99dc4386fb37eb2abe2520337146d0d8540ec244
Haruki Okada <ocadaruma@gmail.com>
2022-09-05 09:31:25 +0900

    dhat

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,7 +3,14 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     let args: Vec<String> = env::args().collect();
 
     let path = &args[1];
@@ -25,10 +32,12 @@ fn main() {
                 // let sample: ExecutionSample = from_event(&event).unwrap();
                 // os_name_total_length += sample.sampled_thread.unwrap().os_name.unwrap().len();
 
-                let thread_name = event.value()
+                let thread_name = event
+                    .value()
                     .get_field("sampledThread")
                     .and_then(|v| v.get_field("osName"))
-                    .and_then(|v| <&str>::try_from(v.value).ok()).unwrap();
+                    .and_then(|v| <&str>::try_from(v.value).ok())
+                    .unwrap();
                 os_name_total_length += thread_name.len();
 
                 event_count += 1;
